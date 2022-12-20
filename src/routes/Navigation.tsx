a83d2c370c43a513f5da4fde85e8f880a64ffef8
Anderson Castiblanco <anderson@pandas.com.co>
2022-12-19 19:07:19 -0500

    Adding lazy components

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,42 +1,46 @@
+import { Suspense } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Routes, Route, NavLink, Navigate } from 'react-router-dom';
 // import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages/'; // ya no los uso porque lo tengo definidios en routes.
 import { routes } from './routes';
 
+
 import logo from '../logo.svg'
 
 export const Navigation = () => {
     return (
-        <BrowserRouter>
-            <div className="main-layout">
-                <nav>
-                    <img src={ logo } alt="React Logo" />
-                    <ul>
-                        { routes.map( ({to, name}) => (
-                            <li key={ to }>
-                            <NavLink 
-                                to={ to } 
-                                className={ ({ isActive }) => isActive ? 'nav-active' : '' }>
-                                    { name }
-                            </NavLink>
-                            </li>
-                        )) }
-                    </ul>
-                </nav>
+        <Suspense fallback={<span>Loading...</span>}>
+            <BrowserRouter>
+                <div className="main-layout">
+                    <nav>
+                        <img src={ logo } alt="React Logo" />
+                        <ul>
+                            { routes.map( ({to, name}) => (
+                                <li key={ to }>
+                                <NavLink 
+                                    to={ to } 
+                                    className={ ({ isActive }) => isActive ? 'nav-active' : '' }>
+                                        { name }
+                                </NavLink>
+                                </li>
+                            )) }
+                        </ul>
+                    </nav>
 
 
-                <Routes>
-                    { routes.map( ({path, Component}) => (
-                        <Route 
-                            key={path}
-                            path={path} 
-                            element={ <Component/> } />
-                    ))}                    
+                    <Routes>
+                        { routes.map( ({path, Component}) => (
+                            <Route 
+                                key={path}
+                                path={path} 
+                                element={ <Component/> } />
+                        ))}                    
 
-                    <Route path="/*" element={ <Navigate to={ routes[0].to} replace /> } />
-                </Routes>
+                        <Route path="/*" element={ <Navigate to={ routes[0].to} replace /> } />
+                    </Routes>
 
-            </div>
-        </BrowserRouter>
+                </div>
+            </BrowserRouter>
+        </Suspense>
     )
 }
